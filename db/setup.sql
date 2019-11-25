5bc1876ea14a5b3d096581537599be0ec70bd188
Nikolas Patrik <nikopatrik17@gmail.com>
2019-11-25 21:12:30 +0100

    Added Initialization Script For Database

---
+++ db/setup.sql
@@ -0,0 +1,12 @@
+SET NAMES utf8;
+
+CREATE TABLE restaurants(
+  name VARCHAR(50),
+  adresa VARCHAR(50)
+);
+
+INSERT INTO restaurants VALUES ("Bucheck", "Zelny trh");
+INSERT INTO restaurants VALUES ("Amici", "Palackeho trida");
+INSERT INTO restaurants VALUES ("Roburrito", "Židenice");
+INSERT INTO restaurants VALUES ("McDonalds", "Vankovka");
+INSERT INTO restaurants VALUES ("BurgerKING", "Vankovka");
